4b36170a7f0eb192b8026d54261c2b7de3900317
Elia Harmouche <Elia Harmouche eliaharm"@users.noreply.github.com>
2021-04-19 16:42:16 +0300

    Update after New CV 1 page

--- public/app.js
+++ public/app.js
@@ -1,1 +1,270 @@
-!function(){"use strict";var e="undefined"==typeof global?self:global;if("function"!=typeof e.require){var t={},n={},i={},r={}.hasOwnProperty,o=/^\.\.?(\/|$)/,a=function(e,t){for(var n,i=[],r=(o.test(t)?e+"/"+t:t).split("/"),a=0,s=r.length;a<s;a++)n=r[a],".."===n?i.pop():"."!==n&&""!==n&&i.push(n);return i.join("/")},s=function(e){return e.split("/").slice(0,-1).join("/")},c=function(t){return function(n){var i=a(s(t),n);return e.require(i,t)}},l=function(e,t){var i=m&&m.createHot(e),r={id:e,exports:{},hot:i};return n[e]=r,t(r.exports,c(e),r),r.exports},f=function(e){var t=i[e];return t&&e!==t?f(t):e},u=function(e,t){return f(a(s(e),t))},d=function(e,i){null==i&&(i="/");var o=f(e);if(r.call(n,o))return n[o].exports;if(r.call(t,o))return l(o,t[o]);throw new Error("Cannot find module '"+e+"' from '"+i+"'")};d.alias=function(e,t){i[t]=e};var p=/\.[^.\/]+$/,h=/\/index(\.[^\/]+)?$/,v=function(e){if(p.test(e)){var t=e.replace(p,"");r.call(i,t)&&i[t].replace(p,"")!==t+"/index"||(i[t]=e)}if(h.test(e)){var n=e.replace(h,"");r.call(i,n)||(i[n]=e)}};d.register=d.define=function(e,i){if(e&&"object"==typeof e)for(var o in e)r.call(e,o)&&d.register(o,e[o]);else t[e]=i,delete n[e],v(e)},d.list=function(){var e=[];for(var n in t)r.call(t,n)&&e.push(n);return e};var m=e._hmr&&new e._hmr(u,d,t,n);d._cache=n,d.hmr=m&&m.wrap,d.brunch=!0,e.require=d}}(),function(){"undefined"==typeof window?this:window;require.register("index.html",function(e,t,n){}),require.register("initialize.js",function(e,t,n){"use strict";document.addEventListener("DOMContentLoaded",function(){console.log("Initialized app")})}),require.register("scripts.js",function(e,t,n){"use strict";!function(e){e("html").removeClass("no-js"),e("header a").click(function(t){if(!e(this).hasClass("no-scroll")){t.preventDefault();var n=e(this).attr("href"),i=e(n).offset().top;e("html, body").animate({scrollTop:i+"px"},Math.abs(window.pageYOffset-e(n).offset().top)/1),e("header").hasClass("active")&&e("header, body").removeClass("active")}}),e("#to-top").click(function(){e("html, body").animate({scrollTop:0},500)}),e("#lead-down span").click(function(){var t=e("#lead").next().offset().top;e("html, body").animate({scrollTop:t+"px"},500)}),e("#experience-timeline").each(function(){$this=e(this),$userContent=$this.children("div"),$userContent.each(function(){e(this).addClass("vtimeline-content").wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>')}),$this.find(".vtimeline-point").each(function(){e(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>')}),$this.find(".vtimeline-content").each(function(){var t=e(this).data("date");t&&e(this).parent().prepend('<span class="vtimeline-date">'+t+"</span>")})}),e("#mobile-menu-open").click(function(){e("header, body").addClass("active")}),e("#mobile-menu-close").click(function(){e("header, body").removeClass("active")}),e("#view-more-projects").click(function(t){t.preventDefault(),e(this).fadeOut(300,function(){e("#more-projects").fadeIn(300)})})}(jQuery)}),require.register("___globals___",function(e,t,n){})}(),require("___globals___");
+(function() {
+  'use strict';
+
+  var globals = typeof global === 'undefined' ? self : global;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+  var aliases = {};
+  var has = {}.hasOwnProperty;
+
+  var expRe = /^\.\.?(\/|$)/;
+  var expand = function(root, name) {
+    var results = [], part;
+    var parts = (expRe.test(name) ? root + '/' + name : name).split('/');
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function expanded(name) {
+      var absolute = expand(dirname(path), name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var hot = hmr && hmr.createHot(name);
+    var module = {id: name, exports: {}, hot: hot};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var expandAlias = function(name) {
+    var val = aliases[name];
+    return (val && name !== val) ? expandAlias(val) : name;
+  };
+
+  var _resolve = function(name, dep) {
+    return expandAlias(expand(dirname(name), dep));
+  };
+
+  var require = function(name, loaderPath) {
+    if (loaderPath == null) loaderPath = '/';
+    var path = expandAlias(name);
+
+    if (has.call(cache, path)) return cache[path].exports;
+    if (has.call(modules, path)) return initModule(path, modules[path]);
+
+    throw new Error("Cannot find module '" + name + "' from '" + loaderPath + "'");
+  };
+
+  require.alias = function(from, to) {
+    aliases[to] = from;
+  };
+
+  var extRe = /\.[^.\/]+$/;
+  var indexRe = /\/index(\.[^\/]+)?$/;
+  var addExtensions = function(bundle) {
+    if (extRe.test(bundle)) {
+      var alias = bundle.replace(extRe, '');
+      if (!has.call(aliases, alias) || aliases[alias].replace(extRe, '') === alias + '/index') {
+        aliases[alias] = bundle;
+      }
+    }
+
+    if (indexRe.test(bundle)) {
+      var iAlias = bundle.replace(indexRe, '');
+      if (!has.call(aliases, iAlias)) {
+        aliases[iAlias] = bundle;
+      }
+    }
+  };
+
+  require.register = require.define = function(bundle, fn) {
+    if (bundle && typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has.call(bundle, key)) {
+          require.register(key, bundle[key]);
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+      delete cache[bundle];
+      addExtensions(bundle);
+    }
+  };
+
+  require.list = function() {
+    var list = [];
+    for (var item in modules) {
+      if (has.call(modules, item)) {
+        list.push(item);
+      }
+    }
+    return list;
+  };
+
+  var hmr = globals._hmr && new globals._hmr(_resolve, require, modules, cache);
+  require._cache = cache;
+  require.hmr = hmr && hmr.wrap;
+  require.brunch = true;
+  globals.require = require;
+})();
+
+(function() {
+var global = typeof window === 'undefined' ? this : window;
+var __makeRelativeRequire = function(require, mappings, pref) {
+  var none = {};
+  var tryReq = function(name, pref) {
+    var val;
+    try {
+      val = require(pref + '/node_modules/' + name);
+      return val;
+    } catch (e) {
+      if (e.toString().indexOf('Cannot find module') === -1) {
+        throw e;
+      }
+
+      if (pref.indexOf('node_modules') !== -1) {
+        var s = pref.split('/');
+        var i = s.lastIndexOf('node_modules');
+        var newPref = s.slice(0, i).join('/');
+        return tryReq(name, newPref);
+      }
+    }
+    return none;
+  };
+  return function(name) {
+    if (name in mappings) name = mappings[name];
+    if (!name) return;
+    if (name[0] !== '.' && pref) {
+      var val = tryReq(name, pref);
+      if (val !== none) return val;
+    }
+    return require(name);
+  }
+};
+require.register("index.html", function(exports, require, module) {
+
+});
+
+;require.register("initialize.js", function(exports, require, module) {
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  // do your setup here
+  console.log('Initialized app');
+});
+});
+
+require.register("scripts.js", function(exports, require, module) {
+'use strict';
+
+/*!
+    Title: Dev Portfolio Template
+    Version: 1.2.2
+    Last Change: 03/25/2020
+    Author: Ryan Fitzgerald
+    Repo: https://github.com/RyanFitzgerald/devportfolio-template
+    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
+
+    Description: This file contains all the scripts associated with the single-page
+    portfolio website.
+*/
+
+(function ($) {
+
+    // Remove no-js class
+    $('html').removeClass('no-js');
+
+    // Animate to section when nav is clicked
+    $('header a').click(function (e) {
+
+        // Treat as normal link if no-scroll class
+        if ($(this).hasClass('no-scroll')) return;
+
+        e.preventDefault();
+        var heading = $(this).attr('href');
+        var scrollDistance = $(heading).offset().top;
+
+        $('html, body').animate({
+            scrollTop: scrollDistance + 'px'
+        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+
+        // Hide the menu once clicked if mobile
+        if ($('header').hasClass('active')) {
+            $('header, body').removeClass('active');
+        }
+    });
+
+    // Scroll to top
+    $('#to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    // Scroll to first element
+    $('#lead-down span').click(function () {
+        var scrollDistance = $('#lead').next().offset().top;
+        $('html, body').animate({
+            scrollTop: scrollDistance + 'px'
+        }, 500);
+    });
+
+    // Create timeline
+    $('#experience-timeline').each(function () {
+
+        $this = $(this); // Store reference to this
+        $userContent = $this.children('div'); // user content
+
+        // Create each timeline block
+        $userContent.each(function () {
+            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+        });
+
+        // Add icons to each block
+        $this.find('.vtimeline-point').each(function () {
+            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+        });
+
+        // Add dates to the timeline if exists
+        $this.find('.vtimeline-content').each(function () {
+            var date = $(this).data('date');
+            if (date) {
+                // Prepend if exists
+                $(this).parent().prepend('<span class="vtimeline-date">' + date + '</span>');
+            }
+        });
+    });
+
+    // Open mobile menu
+    $('#mobile-menu-open').click(function () {
+        $('header, body').addClass('active');
+    });
+
+    // Close mobile menu
+    $('#mobile-menu-close').click(function () {
+        $('header, body').removeClass('active');
+    });
+
+    // Load additional projects
+    $('#view-more-projects').click(function (e) {
+        e.preventDefault();
+        $(this).fadeOut(300, function () {
+            $('#more-projects').fadeIn(300);
+        });
+    });
+})(jQuery);
+});
+
+require.register("___globals___", function(exports, require, module) {
+  
+});})();require('___globals___');
+
+
+//# sourceMappingURL=app.js.map
